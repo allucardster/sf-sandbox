55ee56ec89811aad3790c4e842552f4c48f5b8a7
Richard Melo <allucardster@gmail.com>
2022-09-09 02:31:08 -0500

    Add `symfony/profiler-pack`

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
